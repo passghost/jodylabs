9161198304760ac569f8a603ffc12727d579d1b5
passghost <takeoff@techie.com>
2025-07-28 14:46:19 -0400

    jousti fix

--- Jousti/game.js
+++ Jousti/game.js
@@ -17,15 +17,6 @@ const PLATFORM_SHADOW = 'rgba(255,0,0,0.18)';
 
 // Sparkle animation state
 let sparkleFrame = 0;
-// Rectangle collision detection
-function rectsCollide(a, b) {
-    return (
-        a.x < b.x + b.w &&
-        a.x + a.w > b.x &&
-        a.y < b.y + b.h &&
-        a.y + a.h > b.y
-    );
-}
 
 // Running animation state
 let runFrame = 0;
@@ -135,8 +126,7 @@ function spawnEnemies() {
     }
 }
 spawnEnemies();
-// Player update logic
-function updatePlayer() {
+    }
     // Clamp max speed
     if (player.vx > PLAYER_SPEED) player.vx = PLAYER_SPEED;
     if (player.vx < -PLAYER_SPEED) player.vx = -PLAYER_SPEED;
@@ -182,7 +172,6 @@ function updatePlayer() {
 
     // Animate wing flap
     if (player.flapAnim > 0) player.flapAnim--;
-}
 }
 
 
