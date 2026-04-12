6f62800395c0f8bee3561feab796ca8aa0848158
passghost <takeoff@techie.com>
2026-04-12 14:55:05 -0400

    Wipe repository contents

--- 404museum/404museum.bundle.js
+++
@@ -1,8 +0,0 @@
-
-import * as patterns from './404museumPatterns.js';
-import './404museum.js';
-
-// Attach all pattern functions to window for global access
-for (const [key, value] of Object.entries(patterns)) {
-    window[key] = value;
-}
